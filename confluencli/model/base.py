916086b88b5f2aa8217ea6d7eac3112fad911653
sinmai <bonboyaju19@gmail.com>
2020-08-23 01:02:53 +0900

    separate a function between model and loading

--- confluencli/model/base.py
+++ confluencli/model/base.py
@@ -1,6 +1,5 @@
-import os
 from dataclasses import dataclass
-from confluencli.util import log, handler
+from confluencli.util import log, handler, config
 
 
 logger = log.get_logger()
@@ -25,18 +24,5 @@ class Base(metaclass=Singleton):
     base_url: str = ""
 
     def __post_init__(self):
-        self.__set_credential()
-        self.__set_base_url()
-
-    def __set_credential(self):
-        if env_type.WIKI_USERNAME_ENV.value not in os.environ:
-            handler.handle_error(error_type.CONFLUENCLI_USERNAME_NOT_EXIST)
-        if env_type.WIKI_PASSWORD_ENV.value not in os.environ:
-            handler.handle_error(error_type.CONFLUENCLI_PASSWORD_NOT_EXIST)
-        self.username = os.getenv(env_type.WIKI_USERNAME_ENV.value)
-        self.password = os.getenv(env_type.WIKI_PASSWORD_ENV.value)
-
-    def __set_base_url(self):
-        if env_type.WIKI_BASEURL_ENV.value not in os.environ:
-            handler.handle_error(error_type.CONFLUENCLI_BASEURL_NOT_EXIST)
-        self.base_url = os.getenv(env_type.WIKI_BASEURL_ENV.value).rstrip('/')
+        config.set_credential(self)
+        config.set_base_url(self)
---
+++ confluencli/util/config.py
@@ -0,0 +1,21 @@
+import os
+from confluencli.util import log, handler
+
+logger = log.get_logger()
+error_type = handler.ErrorType
+env_type = handler.EnvType
+
+
+def set_credential(base):
+    if env_type.WIKI_USERNAME_ENV.value not in os.environ:
+        handler.handle_error(error_type.CONFLUENCLI_USERNAME_NOT_EXIST)
+    if env_type.WIKI_PASSWORD_ENV.value not in os.environ:
+        handler.handle_error(error_type.CONFLUENCLI_PASSWORD_NOT_EXIST)
+    base.username = os.getenv(env_type.WIKI_USERNAME_ENV.value)
+    base.password = os.getenv(env_type.WIKI_PASSWORD_ENV.value)
+
+
+def set_base_url(base):
+    if env_type.WIKI_BASEURL_ENV.value not in os.environ:
+        handler.handle_error(error_type.CONFLUENCLI_BASEURL_NOT_EXIST)
+    base.base_url = os.getenv(env_type.WIKI_BASEURL_ENV.value).rstrip('/')
